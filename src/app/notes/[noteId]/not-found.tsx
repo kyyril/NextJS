8581d2e03ee3b08febc44b1263cbc66ee42a2209
kyyril <khairilrahman55@gmail.com>
2024-09-04 02:59:44 +0700

    page not fount

---
+++ src/app/notes/[noteId]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <>
+      <h1>not found</h1>
+      <h1>id note error</h1>
+    </>
+  );
+}
--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,3 +1,9 @@
-export default function Note({ params }: { params: { noteId: String } }) {
+import { notFound } from "next/navigation";
+export default function Note({ params }: { params: { noteId: string } }) {
+  {
+    if (parseInt(params.noteId) > 100) {
+      return notFound();
+    }
+  }
   return <h3>note detail {params.noteId}</h3>;
 }
